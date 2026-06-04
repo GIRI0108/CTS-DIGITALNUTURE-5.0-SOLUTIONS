6f9f6d10f5c3de7437176aca40c4e566a6102871
GIRIVASAN M <radhikagirivasan@gmail.com>
2026-06-04 21:46:01 +0530

    Delete MODULE-2/SQL/Task-24_Average_Session_Duration_Per_Event directory

--- MODULE-2/SQL/Task-24_Average_Session_Duration_Per_Event/task 24.sql
+++
@@ -1,9 +0,0 @@
-USE event_portal;
-
-SELECT 
-    events.title,
-    AVG(TIMESTAMPDIFF(MINUTE, sessions.start_time, sessions.end_time)) AS average_duration_minutes
-FROM events
-JOIN sessions
-ON events.event_id = sessions.event_id
-GROUP BY events.event_id, events.title;
